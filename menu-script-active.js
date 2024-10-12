488bad0a9c4be938c820d41dc255a6884f3c8d7a
MeiliDeBenzhi <159168927+MeiliDeBenzhi@users.noreply.github.com>
2024-10-12 14:05:13 +0200

    Update menu-script-active.js

--- menu-script-active.js
+++ menu-script-active.js
@@ -3,7 +3,7 @@ console.log(document.getElementById('menu')); // This should not be null
 // Add the active class to the active menu link, so it can be highlighted in css
 
 let currentPage = window.location.pathname.split("/").pop(); // Get the current URL path (e.g., "/about.html")
-let menuLinks = document.querySelectorAll("nav ul li a"); // Get all the menu links
+let menuLinks = document.querySelectorAll("header div ul li a"); // Get all the menu links
 // Loop through each menu link and check if its href matches the current page
 menuLinks.forEach(link => {
   let linkPage = link.getAttribute("href");
